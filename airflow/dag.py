b3220629d9323d1dce72842298b50144a8710f1b
nnereidd <drentn123@gmail.com>
2025-04-09 16:57:14 -0700

    first airflow (lambda not working)

--- airflow/dag.py
+++ airflow/dag.py
@@ -24,4 +24,79 @@ dag = DAG(
     tags=['tennis', 'lambda'],
 )
 
+# Config
+S3_BUCKET = 'tennis-predictor-data'
+PLAYER_LIST_KEY = 'raw/rankings/player_list.json'
+
+BATCH_SIZES = {
+    'scrape_player_statistics': 20,
+    'scrape_match_charting_project': 20,
+    'scrape_h2h': 1
+}
+
+# fetch player count and calculate batch sizes 
+@task()
+def fetch_batch_counts():
+    s3 = boto3.client('s3')
+    obj = s3.get_object(Bucket=S3_BUCKET, Key=PLAYER_LIST_KEY)
+    players = json.loads(obj['Body'].read().decode('utf-8'))
+    player_count = len(players)
+
+    batch_info = {}
+    for scraper, batch_size in BATCH_SIZES.items():
+        num_batches = (player_count + batch_size - 1) // batch_size
+        batch_info[scraper] = num_batches
+    return batch_info
+
+@task()
+def generate_batches(scraper_name, batch_info):
+    return [{'scraper': scraper_name, 'batch': i} for i in range(batch_info[scraper_name])]
+
+# call scraping with dynamic batches
+@task()
+def _run_scraper_lambda(batch):
+    client = boto3.client('lambda')
+    client.invoke(
+        FunctionName=batch['scraper'],
+        Payload=json.dumps({"batch": batch['batch']}).encode('utf-8')
+    )
+
+run_scraper_lambda = _run_scraper_lambda
+
+scrape_rankings = LambdaInvokeFunctionOperator(
+    task_id='scrape_rankings',
+    function_name='scrape_rankings',
+    log_type='Tail',
+    dag=dag
+)
+
+cleaning_functions = [
+    'clean_rankings',
+    'clean_player_statistics',
+    'clean_match_charting_project',
+    'clean_h2h'
+]
+
+cleaning_tasks = []
+for cleaner in cleaning_functions:
+    task = LambdaInvokeFunctionOperator(
+        task_id=cleaner,
+        function_name=cleaner,
+        log_type='Tail',
+        dag=dag,
+        trigger_rule=TriggerRule.ALL_SUCCESS
+    )
+    cleaning_tasks.append(task)
+
+# DAG Orchestration 
+
+with dag:
+    batch_info = fetch_batch_counts()
+    batch_info >> scrape_rankings >> cleaning_tasks[0]
+
+    for i, scraper in enumerate(BATCH_SIZES.keys(), start=1):
+        batches = generate_batches.override(task_id=f'generate_{scraper}_batches')(scraper, batch_info)
+        run_scraper_lambda.override(task_id=f'{scraper}_runner').expand(batch=batches) >> cleaning_tasks[i]
+
+
 
